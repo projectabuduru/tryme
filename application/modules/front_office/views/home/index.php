59f3b0a135e34c44c662d7d9dae5300230c9128d
enggar <enggarasmoro3@gmail.com>
2019-09-26 00:34:19 +0700

    front office banner

--- application/modules/front_office/views/home/index.php
+++ application/modules/front_office/views/home/index.php
@@ -2,69 +2,26 @@
 <div class="hero-slider hero-slider-one">
 
     <!-- Single Slide Start -->
-    <div class="single-slide" style="background-image: url(<?php echo base_url('assets/images/slider/slide-bg-1.jpg');?>)">
-        <!-- Hero Content One Start -->
-        <div class="hero-content-one container">
-            <div class="row">
-                <div class="col-lg-10 col-md-10">
-                    <div class="slider-text-info">
-                        <h2>A <span>Different</span> </h2>
-                        <h1>Online <span>Flower</span> Shop </h1>
-                        <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words...</p>
-                        <div class="hero-btn">
-                            <a href="shop.html" class="slider-btn uppercase"><span>SHOP NOW</span></a>
+    <?php foreach ($data['banner'] as $key => $value) { ?>    
+        <div class="single-slide" style="background-image: url(<?php echo base_url($value['image']);?>)">
+            <!-- Hero Content One Start -->
+            <!-- <div class="hero-content-one container">
+                <div class="row">
+                    <div class="col-lg-10 col-md-10">
+                        <div class="slider-text-info">
+                            <h2>A <span>Different</span> </h2>
+                            <h1>Online <span>Flower</span> Shop </h1>
+                            <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words...</p>
+                            <div class="hero-btn">
+                                <a href="shop.html" class="slider-btn uppercase"><span>SHOP NOW</span></a>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> -->
+            <!-- Hero Content One End -->
         </div>
-        <!-- Hero Content One End -->
-    </div>
-    <!-- Single Slide End -->
-
-
-    <!-- Single Slide Start -->
-    <div class="single-slide" style="background-image: url(assets/images/slider/slide-bg-2.jpg))">
-        <!-- Hero Content One Start -->
-        <div class="hero-content-one container">
-            <div class="row">
-                <div class="col-lg-10 col-md-10">
-                    <div class="slider-text-info">
-                        <h2>A <span>Different</span> </h2>
-                        <h1>Online <span>Flower</span> Shop </h1>
-                        <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words...</p>
-                        <div class="hero-btn">
-                            <a href="shop.html" class="slider-btn uppercase"><span>SHOP NOW</span></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Hero Content One End -->
-    </div>
-    <!-- Single Slide End -->
-
-
-    <!-- Single Slide Start -->
-    <div class="single-slide" style="background-image: url(<?php echo base_url('assets/images/slider/slide-bg-3.jpg');?>)">
-        <!-- Hero Content One Start -->
-        <div class="hero-content-one container">
-            <div class="row">
-                <div class="col-lg-10 col-md-10">
-                    <div class="slider-text-info">
-                        <h2>A <span>Different</span> </h2>
-                        <h1>Online <span>Flower</span> Shop </h1>
-                        <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words...</p>
-                        <div class="hero-btn">
-                            <a href="shop.html" class="slider-btn uppercase"><span>SHOP NOW</span></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Hero Content One End -->
-    </div>
-    <!-- Single Slide End -->
+    <?php } ?>
 
 
 </div>
